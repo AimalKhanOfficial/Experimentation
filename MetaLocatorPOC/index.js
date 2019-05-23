ebec3d32bd458df33d7b05ca8dad8a2847edad56
Aimal Khan <aimalkhanofficial@gmail.com>
2019-05-23 14:28:12 -0400

    Meta locator POC.

---
+++ MetaLocatorPOC/index.js
@@ -0,0 +1,8 @@
+var myNavBarText = `
+<ul class='nav nav-tabs'>
+    <li role='presentation' class='active'><a href='#'>Home</a></li>
+    <li role='presentation'><a href='#'>Profile</a></li>
+    <li role='presentation'><a href='#'>Messages</a></li>
+</ul>`;
+
+document.getElementById('codeArea').innerHTML = myNavBarText;
