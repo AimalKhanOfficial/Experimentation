7907a8bd48c0022c08497c08aee0530b4d6b65b6
AimalKhanOfficial <aimalkhanofficial@gmail.com>
2020-02-18 11:56:48 -0500

    Adding swagger.json

--- SwaggerExpress/ReadMe.md
+++ SwaggerExpress/ReadMe.md
@@ -1,1 +1,4 @@
-This doc specifies the steps required to add Swagger (docs) to an Express API
+This doc specifies the steps required to add Swagger (docs) to an Express API:
+* run `npm run swagger-ui-express`
+* add a `swagger.json` file 
+* copy/paste the content from the `swagger.json` file present in this repo.
--- SwaggerExpress/index.js
+++ SwaggerExpress/index.js
@@ -2,8 +2,17 @@ var express = require('express');
 var app = express();
 var port = '2202';
 
-app.get('/', async (req, res, next) => {
-    res.json('Done.');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+
+var options = {
+    explorer: true
+};
+
+app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app.get('/pets', (req, res) => {
+    res.json('Hey');
 });
 
 app.listen(port, () => console.log(`App listening on port ${port}.`));
