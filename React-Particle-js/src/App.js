951515d50fdd2086cb29281e910ed425d9cce388
AimalKhanOfficial <aimalkhanofficial@gmail.com>
2021-01-26 11:52:52 -0500

    React Particle Demo

---
+++ React-Particle-js/src/App.js
@@ -0,0 +1,32 @@
+//import logo from './logo.svg';
+import './App.css';
+import Particles from 'react-particles-js';
+
+function App() {
+  return (
+    <div className="App">
+      <Particles
+  params={{
+    particles: {
+      color: {
+        value: "#000000"
+      },
+      line_linked: {
+        color: {
+          value: "#000000"
+        }
+      },
+      number: {
+        value: 50
+      },
+      size: {
+        value: 3
+      }
+    }
+  }}
+/>
+    </div>
+  );
+}
+
+export default App;
